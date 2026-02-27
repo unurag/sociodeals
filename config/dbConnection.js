660dc37e05553dc8ea5ca776dc6e96361017dbdd
Anurag Singh <anuragsingh@Anurags-MacBook-Air.local>
2026-02-28 02:16:49 +0530

    updated errorHandlerj

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,22 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
-      serverSelectionTimeoutMS: 5000, // fail fast (default is 30s)
-      socketTimeoutMS: 45000,
-      family: 4, // 🔥 force IPv4 (important for DO + Atlas)
-    });
-
-    console.log(
-      "Database connected",
-      connect.connection.host,
-      connect.connection.name
-    );
-  } catch (error) {
-    console.error("MongoDB Connection Error:", error.message);
-    process.exit(1);
-  }
+    try {
+        const connect = await mongoose.connect("mongodb+srv://unurag:admin@cluster0.tjnuduv.mongodb.net/sociodeals?appName=Cluster0");
+        console.log("Database connected", connect.connection.host, connect.connection.name);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
